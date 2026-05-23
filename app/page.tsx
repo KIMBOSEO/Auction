92f60f54a3db3c646fe136a53ccab483728383be
KBS <bosong111@naver.com>
2026-05-23 15:02:39 +0900

    Test_260523

--- app/page.tsx
+++ app/page.tsx
@@ -49,7 +49,8 @@ export default async function Home(props: Props) {
   }
 
   const { data: items } = await supabaseQuery;
-  const categories = ["전체", "전자기기", "스포츠/레저", "패션/잡화", "취미", "희귀카드", "기타"];
+  //const categories = ["전체", "전자기기", "스포츠/레저", "패션/잡화", "취미", "희귀카드", "기타"];
+  const categories = ["희귀카드"];
 
   return (
     <div className="max-w-6xl mx-auto px-4 py-8 md:py-12">
